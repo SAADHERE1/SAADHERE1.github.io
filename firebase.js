769c6357f6edf701062ed6ab4aa15399864f8f67
Saad Akram <saadakramrafi@gmail.com>
2026-06-29 17:28:05 +0500

    Refactor Firebase config for module import
    Updated Firebase initialization to use module imports.

--- firebase.js
+++ firebase.js
@@ -1,8 +1,19 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBVMN1e9MUsT96JPloRcvtyuu0Px0JT_MU",
-  authDomain: "saad-portfolio-b572d.firebaseapp.com",
-  projectId: "saad-portfolio-b572d",
-  storageBucket: "saad-portfolio-b572d.firebasestorage.app",
-  messagingSenderId: "966248438107",
-  appId: "1:966248438107:web:afdd3fc1be054dd48b627e"
-};
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBVMN1e9MUsT96JPloRcvtyuu0Px0JT_MU",
+    authDomain: "saad-portfolio-b572d.firebaseapp.com",
+    projectId: "saad-portfolio-b572d",
+    storageBucket: "saad-portfolio-b572d.firebasestorage.app",
+    messagingSenderId: "966248438107",
+    appId: "1:966248438107:web:afdd3fc1be054dd48b627e"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
