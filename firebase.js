7a50c3f067319be534947245b610ec9f97e47c9f
Saad Akram <saadakramrafi@gmail.com>
2026-06-29 17:33:00 +0500

    Refactor Firebase initialization and imports

--- firebase.js
+++ firebase.js
@@ -1,18 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+const firebaseConfig = {
+  apiKey: "AIzaSyBVMN1e9MUsT96JPloRcvtyuu0Px0JT_MU",
+  authDomain: "saad-portfolio-b572d.firebaseapp.com",
+  projectId: "saad-portfolio-b572d",
+  storageBucket: "saad-portfolio-b572d.firebasestorage.app",
+  messagingSenderId: "966248438107",
+  appId: "1:966248438107:web:afdd3fc1be054dd48b627e"
+};
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyBVMN1e9MUsT96JPloRcvtyuu0Px0JT_MU",
-    authDomain: "saad-portfolio-b572d.firebaseapp.com",
-    projectId: "saad-portfolio-b572d",
-    storageBucket: "saad-portfolio-b572d.firebasestorage.app",
-    messagingSenderId: "966248438107",
-    appId: "1:966248438107:web:afdd3fc1be054dd48b627e"
-  };
+const app = initializeApp(firebaseConfig);
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, auth, db };
+  
