e856de24734a4e927690fd3a9a237bda2e8974be
Saad Akram <saadakramrafi@gmail.com>
2026-06-29 17:22:16 +0500

    Add Firebase configuration to firebase.js
    Added Firebase configuration object.

--- firebase.js
+++ firebase.js
@@ -1,1 +1,8 @@
-
+const firebaseConfig = {
+  apiKey: "AIzaSyBVMN1e9MUsT96JPloRcvtyuu0Px0JT_MU",
+  authDomain: "saad-portfolio-b572d.firebaseapp.com",
+  projectId: "saad-portfolio-b572d",
+  storageBucket: "saad-portfolio-b572d.firebasestorage.app",
+  messagingSenderId: "966248438107",
+  appId: "1:966248438107:web:afdd3fc1be054dd48b627e"
+};
